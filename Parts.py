6ee2ca8e9243524201d4aa1fb41dcfe832fe83d3
VCM admin account <vcm@vcm-3584.vm.duke.edu>
2018-03-21 23:49:37 -0400

    Add returns to the functions

--- Parts.py
+++ Parts.py
@@ -15,16 +15,22 @@ def create_user(email, age, heart_rate):
     u.heart_rate_times.append(datetime.datetime.now()) # add initial heart rate time
     u.save() # save the user to the database
 
-def user_hear_rate(email):
+def check_user(email):
+    user_status = User.objects.raw({"_id": email}).first()
+
+def user_heart_rate(email):
     user = User.objects.raw({"_id": email}).first()
     user_all_heart_rate = user.heart_rate
+    return user_all_heart_rate
 
 def user_average_heart_rate(user_all_heart_rate):
     average_heart_rate = np.mean(user_all_heart_rate)
+    return average_heart_rate    
 
-def user_heart_rate_time(email, time_interval):
+def user_heart_rate_time(email):
     user = User.object.raw({"_id": email}).first()
     user_time = user.heart_rate_times
+    return user_time
 
 def user_interval_average_heart_rate(time, heart_rate, interval):
     pass
