d2871999bcf75559bd7722861e3667ff1861732d
VCM admin account <vcm@vcm-3584.vm.duke.edu>
2018-04-02 23:01:05 -0400

    Updates

--- Flask_Part.py
+++ Flask_Part.py
@@ -15,30 +15,30 @@ def heart_rate():
     R = request.get_json()
     
     try:
-        Parts.add_heart_rate(R["user_email"], R["heart_rate"], R["time"])
+        Parts.add_heart_rate(R["user_email"], R["user_age"], R["heart_rate"])
     except:
-        Parts.create_user(R["user_email"], R["user_age"], R["heart_rate"])
+         Parts.create_user(R["user_email"], R["user_age"], R["heart_rate"])
     
     return jsonify(R)
 
 @app.route('/api/heart_rate/<user_email>', methods=["GET"])
-def heart_rate_all():
+def heart_rate_all(user_email):
     user_all_heart_rate = Parts.user_heart_rate(user_email) 
     return jsonify(user_all_heart_rate)
 
-@app.route('/api/heart_rate/average/<user_email>', methods=["GET"])
-def average_heart_rate():
-    user_all_heart_rate = Parts.user_heart_rate(user_email)
-    average_heart_rate = Parts.user_average_heart_rate(user_all_heart_rate)
-    return jsonify(average_heart_rate)
+#@app.route('/api/heart_rate/average/<user_email>', methods=["GET"])
+#def average_heart_rate():
+#    user_all_heart_rate = Parts.user_heart_rate(user_email)
+#    average_heart_rate = Parts.user_average_heart_rate(user_all_heart_rate)
+#    return jsonify(average_heart_rate)
 
-@app.route('/api/heart_rate/interval_average', methods=["POST"])
-def interval_average_heart_rate():
-    R = request.get_json()
+#@app.route('/api/heart_rate/interval_average', methods=["POST"])
+#def interval_average_heart_rate():
+#    R = request.get_json()
  
-    interval_heart_rate = Parts.user_interval_average_heart_rate(R["user_email"], R["heart_rate_average_since"])
+#    interval_heart_rate = Parts.user_interval_average_heart_rate(R["user_email"], R["heart_rate_average_since"])
     
-    return jsonify(interval_heart_rate)
+#    return jsonify(interval_heart_rate)
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0")
+#if __name__ == "__main__":
+#    app.run(host="0.0.0.0")
--- client.py
+++ client.py
@@ -3,14 +3,15 @@ import requests
 
 R1 = requests.post("http://vcm-3584.vm.duke.edu:5000/api/heart_rate", json = {"user_email": "ps178@duke.edu", "user_age": 22, "heart_rate": 80})
 
+
 R2 = requests.get("http://vcm-3584.vm.duke.edu:5000/api/heart_rate/ps178@duke.edu")
-name = r1.json()
+#name = R2.json()
 
-R3 = requests.get("http://vcm-3584.vm.duke.edu:5000/api/heart_rate/average/ps178@duke.edu")
-hello = r2.json()
+#R3 = requests.get("http://vcm-3584.vm.duke.edu:5000/api/heart_rate/average/ps178@duke.edu")
+#hello = R3.json()
 
-R4 = requests.post("http://vcm-3584.vm.duke.edu:5000/api/heart_rate/interval_average", json={"user_email": "", "heart_rate_average_since": ""})
+#R4 = requests.post("http://vcm-3584.vm.duke.edu:5000/api/heart_rate/interval_average", json={"user_email": "", "heart_rate_average_since": ""})
 
-
+#print(name, hello)
 
 
